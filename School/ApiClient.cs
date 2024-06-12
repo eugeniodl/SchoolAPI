7149ea672b19abbf52fdc6acb5156b3445834513
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2024-06-12 12:07:14 -0600

    Agregar StudentForm

---
+++ School/ApiClient.cs
@@ -0,0 +1,25 @@
+﻿using SharedModels.Dto;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School
+{
+    public class ApiClient
+    {
+        private readonly HttpClient _httpClient;
+        public IRepository<StudentDto> Students { get; }
+        public IRepository<AttendanceDto> Attendances { get; }
+
+        public ApiClient()
+        {
+            string apiBaseUrl = ConfigurationManager.AppSettings["ApiBaseUrl"];
+            _httpClient = new HttpClient { BaseAddress = new Uri(apiBaseUrl)};
+            Students = new Repository<StudentDto>(_httpClient, "Student");
+            Attendances = new Repository<AttendanceDto>(_httpClient, "Attendance");
+        }
+    }
+}
---
+++ School/StudentForm.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace School
+{
+    partial class StudentForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            txtName = new TextBox();
+            chkRegistered = new CheckBox();
+            btnAdd = new Button();
+            btnUpdate = new Button();
+            btnDelete = new Button();
+            dgvStudent = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dgvStudent).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(231, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(323, 38);
+            label1.TabIndex = 0;
+            label1.Text = "DATOS DEL ESTUDIANTE";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(46, 71);
+            label2.Name = "label2";
+            label2.Size = new Size(67, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Nombre:";
+            // 
+            // txtName
+            // 
+            txtName.Location = new Point(125, 68);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(387, 27);
+            txtName.TabIndex = 2;
+            // 
+            // chkRegistered
+            // 
+            chkRegistered.AutoSize = true;
+            chkRegistered.Location = new Point(610, 67);
+            chkRegistered.Name = "chkRegistered";
+            chkRegistered.Size = new Size(103, 24);
+            chkRegistered.TabIndex = 3;
+            chkRegistered.Text = "Registrado";
+            chkRegistered.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(46, 115);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(94, 29);
+            btnAdd.TabIndex = 4;
+            btnAdd.Text = "Agregar";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.Location = new Point(231, 115);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(94, 29);
+            btnUpdate.TabIndex = 5;
+            btnUpdate.Text = "Actualizar";
+            btnUpdate.UseVisualStyleBackColor = true;
+            btnUpdate.Click += btnUpdate_Click;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(418, 115);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(94, 29);
+            btnDelete.TabIndex = 6;
+            btnDelete.Text = "Eliminar";
+            btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // dgvStudent
+            // 
+            dgvStudent.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvStudent.Location = new Point(46, 171);
+            dgvStudent.Name = "dgvStudent";
+            dgvStudent.ReadOnly = true;
+            dgvStudent.RowHeadersWidth = 51;
+            dgvStudent.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvStudent.Size = new Size(665, 235);
+            dgvStudent.TabIndex = 7;
+            dgvStudent.CellClick += dgvStudent_CellClick;
+            // 
+            // StudentForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(751, 450);
+            Controls.Add(dgvStudent);
+            Controls.Add(btnDelete);
+            Controls.Add(btnUpdate);
+            Controls.Add(btnAdd);
+            Controls.Add(chkRegistered);
+            Controls.Add(txtName);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "StudentForm";
+            Load += StudentForm_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvStudent).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtName;
+        private CheckBox chkRegistered;
+        private Button btnAdd;
+        private Button btnUpdate;
+        private Button btnDelete;
+        private DataGridView dgvStudent;
+    }
+}
