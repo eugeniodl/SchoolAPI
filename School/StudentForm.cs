a6e393fb72807ff197aaae2096ab3a951d6e3a5c
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2024-06-13 11:32:59 -0600

    Agregar método para eliminar

--- School/StudentForm.cs
+++ School/StudentForm.cs
@@ -80,7 +80,7 @@ namespace School
 
         private async void btnUpdate_Click(object sender, EventArgs e)
         {
-            if(dgvStudent.SelectedRows.Count > 0)
+            if (dgvStudent.SelectedRows.Count > 0)
             {
                 var selectedStudent = (StudentDto)dgvStudent.SelectedRows[0].DataBoundItem;
                 var updateStudent = new StudentUpdateDto
@@ -118,6 +118,47 @@ namespace School
             else
             {
                 MessageBox.Show("Seleccione un estudiante para actualizar.",
+                    "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private async void btnDelete_Click(object sender, EventArgs e)
+        {
+            if(dgvStudent.SelectedRows.Count > 0)
+            {
+                var selectedStudent = (StudentDto)dgvStudent.SelectedRows[0].DataBoundItem;
+                var result = MessageBox.Show($"¿Está seguro de que desea eliminar el " +
+                    $"estudiante '{selectedStudent.Name}'?", "Confirmación", 
+                    MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (result == DialogResult.Yes)
+                {
+                    try
+                    {
+                        var sucess = 
+                            await _apiClient.Students.DeleteAsync(selectedStudent.StudentId);
+
+                        if (sucess)
+                        {
+                            MessageBox.Show("¡Estudiante eliminado exitosamente!", "¡Éxito!",
+                                MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            await LoadStudentsAsync();
+                        }
+                        else
+                        {
+                            MessageBox.Show($"Error al eliminar estudiante.", "Error",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Error al eliminar estudiante: {ex.Message}", 
+                            "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Seleccione un estudiante para eliminar.",
                     "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
         }
