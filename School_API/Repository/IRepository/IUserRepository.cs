93407441b7c15d600e90ba2b37d72c0d21ddabbf
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2024-06-18 08:33:10 -0600

    Autenticación básica

---
+++ School_API/Repository/IRepository/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using SharedModels;
+
+namespace School_API.Repository.IRepository
+{
+    public interface IUserRepository
+    {
+        Task<User> GetUserByUsernameAsync(string username);
+        Task<User> GetUserByIdAsync(int id);
+        Task<bool> ValidateUserAsync(string username, string password);
+        Task RegisterUserAsync(User user, string password);
+    }
+}
---
+++ School_API/Repository/UserRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using School_API.Data;
+using School_API.Repository.IRepository;
+using SharedModels;
+
+namespace School_API.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly SchoolContext _context;
+        private readonly IPasswordHasher<User> _passwordHasher;
+
+        public UserRepository(SchoolContext context,
+            IPasswordHasher<User> passwordHasher)
+        {
+            _context = context;
+            _passwordHasher = passwordHasher;
+        }
+
+        public async Task<User> GetUserByIdAsync(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task<User> GetUserByUsernameAsync(string username)
+        {
+            return 
+                await _context.Users.SingleOrDefaultAsync(u => u.UserName == username);
+        }
+
+        public async Task RegisterUserAsync(User user, string password)
+        {
+            user.PasswordHash = _passwordHasher.HashPassword(user, password);
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> ValidateUserAsync(string username, string password)
+        {
+            var user = await GetUserByUsernameAsync(username);
+            if(user == null)
+            {
+                return false;
+            }
+            var verificationResult = _passwordHasher.VerifyHashedPassword(user, 
+                user.PasswordHash, password);
+            return verificationResult == PasswordVerificationResult.Success;
+        }
+    }
+}
